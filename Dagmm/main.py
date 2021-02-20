ee6d71aac1d8661a58b0b9449e4ec5fe9909f9c2
xuanhao-chen <32995432+xuanhao-chen@users.noreply.github.com>
2021-02-20 10:44:43 +0800

    Add files via upload

---
+++ Dagmm/main.py
@@ -0,0 +1,177 @@
+import sys
+sys.path.append('../')
+import os
+
+from Dagmm.dagmm import *
+import numpy as np
+import os
+from Dagmm.eval_methods import *
+from sklearn.preprocessing import MinMaxScaler
+#https://github.com/datamllab/pyodds/blob/b79ea797dca104b12df5ff03ba701a024e36deac/pyodds/utils/importAlgorithm.py
+import time
+
+def proprocess(df):
+    df = np.asarray(df, dtype=np.float32)
+
+    if len(df.shape) == 1:
+        raise ValueError("Data must be 2-D array")
+
+    if np.any(sum(np.isnan(df)) != 0):
+        print("Data contains nan. Will be repalced with 0")
+
+        df = np.nan_to_num()
+
+    df = MinMaxScaler().fit_transform(df)
+
+    print("Data is normalized [0,1]")
+
+    return df
+
+
+def anomaly_detection(data_type, num_epochs, gpu, device):
+
+    with open('./results/detection_results_' + str(data_type) +'.txt', 'w') as f:
+        f.write('channel' + '\t' + 'dataset' + '\t' + 'f1' + '\t' + 'pre' + '\t' + 'rec' + '\t' +
+                'tp' + '\t' + 'tn' + '\t' + 'fp' + '\t' + 'fn' + '\t' + 'train_time' + '\t' + 'epoch_time' +
+                 '\t' +'test_time' + '\t' + 'latency' + '\n')
+
+        total_tp = 0.0
+        total_tn = 0.0
+        total_fp = 0.0
+        total_fn = 0.0
+        total_latency = 0.0
+        total_train_time = 0.0
+        total_test_time = 0.0
+        total_epoch_time = 0.0
+
+        path_train = os.path.join(os.path.dirname(os.getcwd()), "datasets", "train", data_type)
+
+        files = os.listdir(path_train)
+        file_number = 0
+        for file in files:
+            file_number += 1
+
+            data_name = data_type + '/' + str(file)
+            print('file=', data_name)
+
+            contamination = 0.1
+
+            model = DAGMM(num_epochs = num_epochs, device=device, contamination=contamination, lambda_energy=0.1, gpu=gpu,
+                          lambda_cov_diag=0.005, lr=1e-3, batch_size=50, gmm_k=3, normal_percentile=80, sequence_length=30, autoencoder_args=None)
+
+            train = np.load('../datasets/train/' + data_name, allow_pickle=True).astype(float)
+            print(train.shape)
+            train_data = proprocess(train)
+
+            test = np.load('../datasets/test/' + data_name, allow_pickle=True).astype(float)
+            print(test.shape)
+            test_data = proprocess(test)
+            label = np.load('../datasets/test_label/' + data_name, allow_pickle=True).astype(float)
+            print(label.shape)
+
+
+            train_start_time = time.time()
+            model.fit(train_data)
+            train_end_time = time.time()
+
+            train_time = train_end_time - train_start_time
+            epoch_time = train_time / num_epochs
+
+
+            test_start_time = time.time()
+            outlierness_score = model.decision_function(test_data)
+            test_time = (time.time() - test_start_time) / len(outlierness_score)
+
+            score = (outlierness_score - min(outlierness_score)) / (max(outlierness_score) - min(outlierness_score))
+
+            t, th = bf_search(score, label)
+
+            print('best_f1:', t[0], 'pre:', t[1], 'rec:', t[2], 'TP:', t[3], 'TN:', t[4], 'FP:', t[5], 'FN:', t[6],
+                  'latency:', t[7], 'threshold:', th)
+
+            f1 = t[0]
+            pre = t[1]
+            rec = t[2]
+            tp = t[3]
+            tn = t[4]
+            fp = t[5]
+            fn = t[6]
+            latency = t[7]
+
+            total_tp += tp
+            total_tn += tn
+            total_fp += fp
+            total_fn += fn
+            total_latency += latency
+            total_pre = total_tp / (total_tp + total_fp)
+            total_rec = total_tp / (total_tp + total_fn)
+            total_f1 = 2*total_pre*total_rec / (total_pre + total_rec)
+            total_train_time += train_time
+            total_test_time += test_time
+            total_epoch_time += epoch_time
+
+            print(str(data_type) + '\t' + str(file) + '\tf1=' + str(f1) + '\tpre=' + str(pre) +
+                  '\trec=' + str(rec) + '\ttp=' + str(tp) + '\ttn=' + str(tn) + '\tfp=' + str(fp) +
+                  '\tfn=' + str(fn) + '\tlatency=' + str(latency))
+
+            print('total results:' + '\tt_f1=' + str(total_f1) + '\tt_pre=' + str(total_pre) +
+                  '\tt_rec=' + str(total_rec) + '\tt_tp=' + str(total_tp) + '\tt_tn=' + str(total_tn) +
+                  '\tt_fp=' + str(total_fp) + '\tt_fn=' + str(total_fn) + '\tt_latency=' + str(total_latency)
+                  + '\tt_epoch_time=' + str(total_epoch_time) + '\tt_test_time=' + str(total_test_time / file_number))
+
+            f.write(str(data_type) + '\t' + str(file) + '\t' + str(f1) + '\t' + str(pre) + '\t' +
+                    str(rec) + '\t' + str(tp) + '\t' + str(tn) + '\t' + str(fp) + '\t' + str(fn) +
+                    '\t' + str(train_time) + '\t' + str(epoch_time) + '\t' + str(test_time) +
+                    '\t' + str(latency) + '\n')
+
+        f.write('\n')
+        f.write('total results' + '\t' + str(data_type) + '\t' + str(total_f1) + '\t' + str(total_pre) + '\t' +
+                str(total_rec) + '\t' + str(total_tp) + '\t' + str(total_tn) + '\t' + str(total_fp) + '\t' + str(
+            total_fn) +
+                '\t' + str(total_train_time) + '\t' + str(total_epoch_time) + '\t' + str(total_test_time / file_number) +
+                '\t' + str(total_latency) + '\n')
+
+
+    print('finished')
+
+
+
+
+num_epochs = 1
+gpu = None
+data_type = 'SMAP'
+
+# device = torch.device("cuda:" + str(gpu) if torch.cuda.is_available() else "cpu")
+device = "cpu"
+print(device)
+anomaly_detection(data_type, num_epochs, gpu, device)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Dagmm/solver.py
@@ -0,0 +1,257 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import os
+import time
+import datetime
+from torch.autograd import grad
+from torch.autograd import Variable
+from .model import *
+import matplotlib.pyplot as plt
+from .utils import *
+from data_loader import *
+import IPython
+from tqdm import tqdm
+
+
+class Solver(object):
+    DEFAULTS = {}
+
+    def __init__(self, data_loader, config):
+        # Data loader
+        self.__dict__.update(Solver.DEFAULTS, **config)
+        self.data_loader = data_loader
+
+        # Build tensorboard if use
+        self.build_model()
+        if self.use_tensorboard:
+            self.build_tensorboard()
+
+        # Start with trained model
+        if self.pretrained_model:
+            self.load_pretrained_model()
+
+    def build_model(self):
+        # Define model
+        self.dagmm = DaGMM(self.gmm_k)
+
+        # Optimizers
+        self.optimizer = torch.optim.Adam(self.dagmm.parameters(), lr=self.lr)
+
+        # Print networks
+        self.print_network(self.dagmm, 'DaGMM')
+
+        if torch.cuda.is_available():
+            self.dagmm.cuda()
+
+    def print_network(self, model, name):
+        num_params = 0
+        for p in model.parameters():
+            num_params += p.numel()
+        print(name)
+        print(model)
+        print("The number of parameters: {}".format(num_params))
+
+    def load_pretrained_model(self):
+        self.dagmm.load_state_dict(torch.load(os.path.join(
+            self.model_save_path, '{}_dagmm.pth'.format(self.pretrained_model))))
+
+        print("phi", self.dagmm.phi, "mu", self.dagmm.mu, "cov", self.dagmm.cov)
+
+        print('loaded trained models (step: {})..!'.format(self.pretrained_model))
+
+    def build_tensorboard(self):
+        from logger import Logger
+        self.logger = Logger(self.log_path)
+
+    def reset_grad(self):
+        self.dagmm.zero_grad()
+
+    def to_var(self, x, volatile=False):
+        if torch.cuda.is_available():
+            x = x.cuda()
+        return Variable(x, volatile=volatile)
+
+    def train(self):
+        iters_per_epoch = len(self.data_loader)
+
+        # Start with trained model if exists
+        if self.pretrained_model:
+            start = int(self.pretrained_model.split('_')[0])
+        else:
+            start = 0
+
+        # Start training
+        iter_ctr = 0
+        start_time = time.time()
+
+        self.ap_global_train = np.array([0, 0, 0])
+        for e in range(start, self.num_epochs):
+            for i, (input_data, labels) in enumerate(tqdm(self.data_loader)):
+                iter_ctr += 1
+                start = time.time()
+
+                input_data = self.to_var(input_data)
+
+                total_loss, sample_energy, recon_error, cov_diag = self.dagmm_step(input_data)
+                # Logging
+                loss = {}
+                loss['total_loss'] = total_loss.data.item()
+                loss['sample_energy'] = sample_energy.item()
+                loss['recon_error'] = recon_error.item()
+                loss['cov_diag'] = cov_diag.item()
+
+                # Print out log info
+                if (i + 1) % self.log_step == 0:
+                    elapsed = time.time() - start_time
+                    total_time = ((self.num_epochs * iters_per_epoch) - (e * iters_per_epoch + i)) * elapsed / (
+                                e * iters_per_epoch + i + 1)
+                    epoch_time = (iters_per_epoch - i) * elapsed / (e * iters_per_epoch + i + 1)
+
+                    epoch_time = str(datetime.timedelta(seconds=epoch_time))
+                    total_time = str(datetime.timedelta(seconds=total_time))
+                    elapsed = str(datetime.timedelta(seconds=elapsed))
+
+                    lr_tmp = []
+                    for param_group in self.optimizer.param_groups:
+                        lr_tmp.append(param_group['lr'])
+                    tmplr = np.squeeze(np.array(lr_tmp))
+
+                    log = "Elapsed {}/{} -- {} , Epoch [{}/{}], Iter [{}/{}], lr {}".format(
+                        elapsed, epoch_time, total_time, e + 1, self.num_epochs, i + 1, iters_per_epoch, tmplr)
+
+                    for tag, value in loss.items():
+                        log += ", {}: {:.4f}".format(tag, value)
+
+                    IPython.display.clear_output()
+                    print(log)
+
+                    if self.use_tensorboard:
+                        for tag, value in loss.items():
+                            self.logger.scalar_summary(tag, value, e * iters_per_epoch + i + 1)
+                    else:
+                        plt_ctr = 1
+                        if not hasattr(self, "loss_logs"):
+                            self.loss_logs = {}
+                            for loss_key in loss:
+                                self.loss_logs[loss_key] = [loss[loss_key]]
+                                plt.subplot(2, 2, plt_ctr)
+                                plt.plot(np.array(self.loss_logs[loss_key]), label=loss_key)
+                                plt.legend()
+                                plt_ctr += 1
+                        else:
+                            for loss_key in loss:
+                                self.loss_logs[loss_key].append(loss[loss_key])
+                                plt.subplot(2, 2, plt_ctr)
+                                plt.plot(np.array(self.loss_logs[loss_key]), label=loss_key)
+                                plt.legend()
+                                plt_ctr += 1
+
+                        plt.show()
+
+                    print("phi", self.dagmm.phi, "mu", self.dagmm.mu, "cov", self.dagmm.cov)
+                # Save model checkpoints
+                if (i + 1) % self.model_save_step == 0:
+                    torch.save(self.dagmm.state_dict(),
+                               os.path.join(self.model_save_path, '{}_{}_dagmm.pth'.format(e + 1, i + 1)))
+
+    def dagmm_step(self, input_data):
+        self.dagmm.train()
+        enc, dec, z, gamma = self.dagmm(input_data)
+
+        total_loss, sample_energy, recon_error, cov_diag = self.dagmm.loss_function(input_data, dec, z, gamma,
+                                                                                    self.lambda_energy,
+                                                                                    self.lambda_cov_diag)
+
+        self.reset_grad()
+        total_loss.backward()
+
+        torch.nn.utils.clip_grad_norm_(self.dagmm.parameters(), 5)
+        self.optimizer.step()
+
+        return total_loss, sample_energy, recon_error, cov_diag
+
+    def test(self):
+        print("======================TEST MODE======================")
+        self.dagmm.eval()
+        self.data_loader.dataset.mode = "train"
+
+        N = 0
+        mu_sum = 0
+        cov_sum = 0
+        gamma_sum = 0
+
+        for it, (input_data, labels) in enumerate(self.data_loader):
+            input_data = self.to_var(input_data)
+            enc, dec, z, gamma = self.dagmm(input_data)
+            phi, mu, cov = self.dagmm.compute_gmm_params(z, gamma)
+
+            batch_gamma_sum = torch.sum(gamma, dim=0)
+
+            gamma_sum += batch_gamma_sum
+            mu_sum += mu * batch_gamma_sum.unsqueeze(-1)  # keep sums of the numerator only
+            cov_sum += cov * batch_gamma_sum.unsqueeze(-1).unsqueeze(-1)  # keep sums of the numerator only
+
+            N += input_data.size(0)
+
+        train_phi = gamma_sum / N
+        train_mu = mu_sum / gamma_sum.unsqueeze(-1)
+        train_cov = cov_sum / gamma_sum.unsqueeze(-1).unsqueeze(-1)
+
+        print("N:", N)
+        print("phi :\n", train_phi)
+        print("mu :\n", train_mu)
+        print("cov :\n", train_cov)
+
+        train_energy = []
+        train_labels = []
+        train_z = []
+        for it, (input_data, labels) in enumerate(self.data_loader):
+            input_data = self.to_var(input_data)
+            enc, dec, z, gamma = self.dagmm(input_data)
+            sample_energy, cov_diag = self.dagmm.compute_energy(z, phi=train_phi, mu=train_mu, cov=train_cov,
+                                                                size_average=False)
+
+            train_energy.append(sample_energy.data.cpu().numpy())
+            train_z.append(z.data.cpu().numpy())
+            train_labels.append(labels.numpy())
+
+        train_energy = np.concatenate(train_energy, axis=0)
+        train_z = np.concatenate(train_z, axis=0)
+        train_labels = np.concatenate(train_labels, axis=0)
+
+        self.data_loader.dataset.mode = "test"
+        test_energy = []
+        test_labels = []
+        test_z = []
+        for it, (input_data, labels) in enumerate(self.data_loader):
+            input_data = self.to_var(input_data)
+            enc, dec, z, gamma = self.dagmm(input_data)
+            sample_energy, cov_diag = self.dagmm.compute_energy(z, size_average=False)
+            test_energy.append(sample_energy.data.cpu().numpy())
+            test_z.append(z.data.cpu().numpy())
+            test_labels.append(labels.numpy())
+
+        test_energy = np.concatenate(test_energy, axis=0)
+        test_z = np.concatenate(test_z, axis=0)
+        test_labels = np.concatenate(test_labels, axis=0)
+
+        combined_energy = np.concatenate([train_energy, test_energy], axis=0)
+        combined_labels = np.concatenate([train_labels, test_labels], axis=0)
+
+        thresh = np.percentile(combined_energy, 100 - 20)
+        print("Threshold :", thresh)
+
+        pred = (test_energy > thresh).astype(int)
+        gt = test_labels.astype(int)
+
+        from sklearn.metrics import precision_recall_fscore_support as prf, accuracy_score
+
+        accuracy = accuracy_score(gt, pred)
+        precision, recall, f_score, support = prf(gt, pred, average='binary')
+
+        print("Accuracy : {:0.4f}, Precision : {:0.4f}, Recall : {:0.4f}, F-score : {:0.4f}".format(accuracy, precision,
+                                                                                                    recall, f_score))
+
+        return accuracy, precision, recall, f_score
